8cbcf882c5dfaa5844e830d570971e923e1893c5
jaranedag <jaranedag@udd.cl>
2023-02-25 23:46:53 +0000

    tarea familia api flask araneda

--- src/app.py
+++ src/app.py
@@ -26,18 +26,34 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    response_body={
+        "family":members
     }
+    return jsonify(response_body["family"]), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
 
-    return jsonify(response_body), 200
+    member = jackson_family.get_member(int(id))
+    response_body={
+        "members":member
+    }
+    return jsonify(response_body["members"]), 200
 
+@app.route('/member', methods=['POST'])
+def add_member():
+    jackson_family.add_member(request.get_json())
+    return jsonify({"msg":"Member Added"}), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+
+    return jsonify({"done":True}),  200
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
